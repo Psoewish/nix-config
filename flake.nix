1ad1dd1ac01d0d4753f4a8d76e670edb1a9bafb3
psoewish <psoewish@proton.me>
2025-03-22 15:58:12 +0100

    remove home manager

--- flake.nix
+++ flake.nix
@@ -5,21 +5,6 @@
   # Nix Packages
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  # Home Manager
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-  # Theming with Stylix
-    stylix = {
-      url = "github:danth/stylix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "home-manager";
-      };
-    };
-
   # Zen Browser
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
@@ -27,7 +12,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, stylix, ... } @inputs:
+  outputs = { self, nixpkgs, ... } @inputs:
   let
     system = "x86_64-linux";
     lib = nixpkgs.lib;
@@ -38,17 +23,8 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/desktop/configuration.nix
-          stylix.nixosModules.stylix
           ./system/core
           ./system/packages
-          home-manager.nixosModules.home-manager {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.psoewish = import ./home/home.nix;
-              extraSpecialArgs = {inherit inputs; };
-            };
-          }
         ];
       };
     };
