12c68cc4b449047db54192a9fb17c336c726d5b7
psoewish <psoewish@proton.me>
2025-03-24 15:38:07 +0100

    Change shell to fish

--- modules/home/wezterm.nix
+++ modules/home/wezterm.nix
@@ -5,6 +5,7 @@
     enable = true;
     extraConfig = ''
     local config = {
+      default_prog = { 'fish' },
       font = wezterm.font ('CaskaydiaCove Nerd Font'),
       font_size = 14,
       color_scheme = 'rose-pine',
