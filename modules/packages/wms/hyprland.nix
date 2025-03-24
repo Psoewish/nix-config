e91dc7c925f44a450647564c08622ffcfc6a6e2f
psoewish <psoewish@proton.me>
2025-03-24 15:59:27 +0100

    move hyprland extra packages to hyprland.nix

--- modules/packages/wms/hyprland.nix
+++ modules/packages/wms/hyprland.nix
@@ -1,34 +1,34 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   programs.hyprland = {
     enable = true;
     withUWSM = true;
-    portalPackage = pkgs.xdg-desktop-portal-hyprland;
+    # portalPackage = pkgs.xdg-desktop-portal-hyprland;
   };
 
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+  # environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  # environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
-  xdg.portal = {
-    enable = true;
-    wlr.enable = false;
-    xdgOpenUsePortal = false;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-hyprland
-      pkgs.xdg-desktop-portal-gtk
-    ];
-  };
+  # xdg.portal = {
+  #   enable = true;
+  #   wlr.enable = false;
+  #   xdgOpenUsePortal = false;
+  #   extraPortals = [
+  #     pkgs.xdg-desktop-portal-hyprland
+  #     pkgs.xdg-desktop-portal-gtk
+  #   ];
+  # };
 
-  environment.systemPackages = with pkgs; [
-    hyprcursor
-    hypridle
-    mpvpaper
-    waypaper
-    rofi-wayland
-    grimblast
-    wl-clipboard
-    dunst
-    waybar
-  ];
+  # environment.systemPackages = with pkgs; [
+  #   hyprcursor
+  #   hypridle
+  #   mpvpaper
+  #   waypaper
+  #   rofi-wayland
+  #   grimblast
+  #   wl-clipboard
+  #   dunst
+  #   waybar
+  # ];
 }
