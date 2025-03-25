3f659a9b51588dca6d626403238a8a6efd5970f0
psoewish <psoewish@proton.me>
2025-03-25 14:07:38 +0100

    Fix autostart entry, again

--- modules/home/wms/hyprland/autostart.nix
+++ modules/home/wms/hyprland/autostart.nix
@@ -5,7 +5,7 @@
     exec-once = [
       "hyprctl setcursor Bibata-Modern-Ice 24"
       "sleep 1; exec uwsm app -- discord"
-      "sleep 5; exec uwsm app -- Cider"
+      "sleep 5; exec uwsm app -- cider-2"
       "uwsm app -- steam -silent"
     ];
   };
