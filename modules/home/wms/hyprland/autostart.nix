1e7de34106092ba8b703fc7695de69630b670571
psoewish <psoewish@proton.me>
2025-03-25 02:07:53 +0100

    Fix autostart commands

--- modules/home/wms/hyprland/autostart.nix
+++ modules/home/wms/hyprland/autostart.nix
@@ -4,8 +4,8 @@
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       "hyprctl setcursor Bibata-Modern-Ice 24"
-      "sleep 1; exec uwsm app -- nixtop"
-      "sleep 5; exec uwsm app -- cider"
+      "sleep 1; exec uwsm app -- discord"
+      "sleep 5; exec uwsm app -- Cider"
       "uwsm app -- steam -silent"
     ];
   };
