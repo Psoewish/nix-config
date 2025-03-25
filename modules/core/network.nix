755445f3fb2247f48aad775032aa3ad24dbc507a
psoewish <psoewish@proton.me>
2025-03-25 15:03:38 +0100

    Enable resolved service

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -6,6 +6,13 @@
     enableIPv6 = false;
     firewall.enable = false;
   };
+
+  services.resolved = {
+    enable = true;
+    domains = [ "~."];
+    fallbackDns = [ "9.9.9.9" "1.1.1.1" ];
+  };
+
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "client";
