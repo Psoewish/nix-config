e8d4a5f859bf8bd6b832ffdeccdec5079620586a
psoewish <psoewish@proton.me>
2025-03-26 03:22:33 +0100

    Add rebuild script

--- home-manager/terminal/fish.nix
+++ home-manager/terminal/fish.nix
@@ -21,6 +21,33 @@
     '';
     functions = {
       copycat = "cat $argv | wl-copy";
+      build = ''
+        if [ -z $argv ]
+          command echo 'Argument can\'t be empty.'
+          command echo ' '
+          command echo '    --now      |    Rebuilds your system and switched to the new configuration'
+          command echo '    --later    |    Rebuilds your system and switches to the new configuration upon rebooting'
+          command echo '    --test     |    Checks if your configuration is valid'
+        else
+          switch $argv[1]
+            case --now
+              command sudo nixos-rebuild switch --flake ~/nix-config
+            case --later
+              command sudo nixos-rebuild boot --flake ~/nix-config
+            case --test
+              command sudo nixos-rebuild test --flake ~/nix-config
+            case -h
+              command echo '    --now      |    Rebuilds your system and switched to the new configuration'
+              command echo '    --later    |    Rebuilds your system and switches to the new configuration upon rebooting'
+              command echo '    --test     |    Checks if your configuration is valid'
+            case '*'
+              command echo $argv' is not a valid argument'
+              command echo '    --now      |    Rebuilds your system and switched to the new configuration'
+              command echo '    --later    |    Rebuilds your system and switches to the new configuration upon rebooting'
+              command echo '    --test     |    Checks if your configuration is valid'
+            end
+          end
+      '';
     };
     plugins = with pkgs.fishPlugins; [
       { name = "autopair"; src = autopair.src; }
