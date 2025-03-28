57f279976286e68fcd792b5a9e189ac39eabaa04
psoewish <psoewish@proton.me>
2025-03-28 12:17:22 +0100

    Add starship config

--- home-manager/terminal/starship.nix
+++ home-manager/terminal/starship.nix
@@ -1,10 +1,26 @@
-{ ... }:
+{ lib, ... }:
 
 {
   programs.starship = {
     enable = true;
     enableTransience = true;
+    settings = {
+      format = lib.concatStrings [
+        "$directory\\"
+        "$git_branch\\"
+        "$git_status\\"
+        "$line_break\\"
+        "$username\\"
+        "hostname\\"
+        "$nix_shell\\"
+        "$character"
+      ];
+      git_branch.format = "[$branch]($style) ";
+      git_status.format = "[$all_status]($style) ";
+      username.format = "[$user]($style) ";
+      hostname.format = "[$hostname]($style) ";
+      nix_shell.format = "[$name]($style) ";
+    };
   };
-  stylix.targets.starship.enable = false;
 }
   
