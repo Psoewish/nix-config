7b27dc4fea1bf387b0886e57ddbd0a06c8717070
psoewish <psoewish@proton.me>
2025-03-31 23:52:26 +0200

    Fix monitor defitnition typo

--- home-manager/wms/qtile/kanshi.nix
+++ home-manager/wms/qtile/kanshi.nix
@@ -10,12 +10,12 @@
           {
             criteria = "DP-1";
             mode = "2560x1440@144Hz";
-            position = "0,0";
+            position = "0,1440";
           }
           {
-            criteria = "DP-1";
+            criteria = "DP-2";
             mode = "2560x1440@60Hz";
-            position = "0,-1440";
+            position = "0,0";
           }
         ];
       }
