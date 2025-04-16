9b3bc79c30bece5ea04642d03ee279f83a38ce33
psoewish <psoewish@proton.me>
2025-04-16 02:19:51 +0200

    Add some wine/proton tools

--- packages/steam.nix
+++ packages/steam.nix
@@ -2,10 +2,14 @@
   programs.steam = {
     enable = true;
     extraCompatPackages = with pkgs; [proton-ge-bin];
+    protontricks.enable = true;
   };
 
   # Modding stuff
   environment.systemPackages = with pkgs; [
     r2modman
+    steamtinkerlaunch
+    winetricks
+    protontricks
   ];
 }
